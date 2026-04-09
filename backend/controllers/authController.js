e21c43d0d8eeda0e68257aa1966767ac2d65e0b1
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2026-04-09 21:07:03 +0530

    auth controller added

---
+++ backend/controllers/authController.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please provide username and password" });
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    res.json({
+      _id: user._id,
+      username: user.username,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+const getMe = async (req, res) => {
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    role: req.user.role,
+  });
+};
+
+module.exports = { login, getMe };
